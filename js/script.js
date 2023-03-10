c01a5bb2ae59e89486cf6c630b75ee5890bb3404
username <youremail@domain.com>
2023-03-10 03:23:25 -0800

    Uploading cached elements

--- js/script.js
+++ js/script.js
@@ -12,10 +12,15 @@
   const spinCost = 25;
 
   /*----- state variables -----*/
-
-
+  
   /*----- cached elements  -----*/
-
+  const creditsEl = document.querySelector('#credits');
+  const betEl = document.querySelector('#bet');
+  const reel1El = document.querySelector('#reel1');
+  const reel2El = document.querySelector('#reel2');
+  const reel3El = document.querySelector('#reel3');
+  const messageEl = document.querySelector('#message');
+  
 
   /*----- event listeners -----*/
 
